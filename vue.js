c60a06d49be39c14bb1d37ac86060e241af718f6
Tommaso Cavaletti <cavalettitommaso@gmail.com>
2021-05-05 11:14:35 -0400

    scheletro.cambiare logica e usare l'array.

---
+++ vue.js
@@ -0,0 +1,63 @@
+const app = new Vue ({
+    el: "#root",
+    data: {
+        wallpaper: "img/img-1.jpg",
+        counter: 0,
+        wallpaperArray: [
+            "img/img-1.jpg",
+            "img/img-2.jpg",
+            "img/img-3.jpg",
+            "img/img-4.jpg",
+        ]
+    },
+    methods: {
+       changeImagePlus() {
+          this.counter ++
+
+          if (this.counter > 4) {
+              this.counter = 1
+          }
+
+          if (this.counter === 0) {
+              this.wallpaper = "img/img-1.jpg"
+          }
+
+          else if (this.counter === 1) {
+            this.wallpaper = "img/img-2.jpg"
+          }
+
+          else if (this.counter === 2) {
+            this.wallpaper = "img/img-3.jpg"
+          }
+
+          else if (this.counter === 3) {
+            this.wallpaper = "img/img-4.jpg"
+          }
+       },
+
+       changeImageMinus() {
+        this.counter --
+
+        if (0 > this.counter) {
+            this.counter = 3
+        }
+
+        if (this.counter === 0) {
+            this.wallpaper = "img/img-1.jpg"
+        }
+
+        else if (this.counter === 1) {
+          this.wallpaper = "img/img-2.jpg"
+        }
+
+        else if (this.counter === 2) {
+          this.wallpaper = "img/img-3.jpg"
+        }
+
+        else if (this.counter === 3) {
+          this.wallpaper = "img/img-4.jpg"
+        }
+     }
+    }
+})
+
